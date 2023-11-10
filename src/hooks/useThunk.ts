c1b769fa122ef7883eed5fde07577cd28ebc503d
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-09 23:10:46 -0400

    useThunk.ts using async/await syntax.

--- src/hooks/useThunk.ts
+++ src/hooks/useThunk.ts
@@ -5,12 +5,14 @@ const useThunk = (thunk: (...args: any[]) => Promise<any>): [(...args: any[]) =>
   const [error, setError] = useState<Error | null>(null);
 
   const runThunk = useCallback(async (...args: any[]) => {
-    setIsLoading(true);
-
-    thunk(...args)
-      .unwrap()
-      .catch(e => setError(e))
-      .finally(() => setIsLoading(false));
+    try {
+      setIsLoading(true);
+      return await thunk(...args).unwrap();
+    } catch (e) {
+      setError(e);
+    } finally {
+      setIsLoading(false);
+    }
   }, [thunk]);
 
   return [runThunk, isLoading, error];
